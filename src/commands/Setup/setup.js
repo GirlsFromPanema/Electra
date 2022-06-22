f1e29a76b8ba19989ede5c36cd8d77fcc362486a
vKxni <Konradtoesch@icloud.com>
2022-06-22 21:42:46 +0200

    formatting

--- src/commands/Setup/setup.js
+++ src/commands/Setup/setup.js
@@ -4,8 +4,8 @@ const { CommandInteraction, Permissions } = require("discord.js");
 const Guild = require("../../models/guilds.js");
 
 module.exports.cooldown = {
-    length: 10000, /* in ms */
-    users: new Set()
+  length: 10000 /* in ms */,
+  users: new Set(),
 };
 
 /**
@@ -13,58 +13,74 @@ module.exports.cooldown = {
  * @param {CommandInteraction} interaction The Command Interaciton
  * @param {any} utils Additional util
  */
-module.exports.run = async (interaction, utils) =>
-{
-    try
-    {
-        const isSetup = await Guild.findOne({ id: interaction.guildId });
-        if (!isSetup)
-        {
-            const role = interaction.options.getRole("role", true);
-            const channel = interaction.options.getChannel("channel", true);
+module.exports.run = async (interaction, utils) => {
+  try {
+    const isSetup = await Guild.findOne({ id: interaction.guildId });
+    if (!isSetup) {
+      const role = interaction.options.getRole("role", true);
+      const channel = interaction.options.getChannel("channel", true);
 
-            if (channel.type != "GUILD_TEXT")
-            {
-                interaction.reply({ content: "Expected a text channel.", ephemeral: true });
-                return;
-            }
-            if(!role) {
-                interaction.reply({ content: "This is not a valid Role!", ephemeral: true})
-            }
+      if (channel.type != "GUILD_TEXT") {
+        interaction.reply({
+          content: "Expected a text channel.",
+          ephemeral: true,
+        });
+        return;
+      }
+      if (!role)
+        return interaction.reply({
+          content: "This is not a valid Role!",
+          ephemeral: true,
+        });
 
-            const newGuild = new Guild({
-                id: interaction.guildId,
-                role: role.id,
-                channel: channel.id
-            });
-            newGuild.save();
+      const newGuild = new Guild({
+        id: interaction.guildId,
+        role: role.id,
+        channel: channel.id,
+      });
+      newGuild.save();
 
-            interaction.reply({ content: "Guild setup successfully.", ephemeral: true });
-        } else {
-            const role = interaction.options.getRole("role", true);
-            const channel = interaction.options.getChannel("channel", true);
+      interaction.reply({
+        content: "Guild setup successfully.",
+        ephemeral: true,
+      });
+    } else {
+      const role = interaction.options.getRole("role", true);
+      const channel = interaction.options.getChannel("channel", true);
 
-            await Guild.findOneAndUpdate({ 
-                id: interaction.guildId, 
-                channel: channel.id, 
-                role: role.id 
-            })
-            await interaction.reply({ content: `Successfully set the tracking to ${channel} with the ${role} Role.`})
-        }
+      await Guild.findOneAndUpdate({
+        id: interaction.guildId,
+        channel: channel.id,
+        role: role.id,
+      });
+      await interaction.reply({
+        content: `Successfully set the tracking to ${channel} with the ${role} Role.`,
+      });
     }
-    catch (err)
-    {
-        return Promise.reject(err);
-    }
+  } catch (err) {
+    return Promise.reject(err);
+  }
 };
 
 module.exports.permissions = {
-    clientPermissions: [Permissions.FLAGS.SEND_MESSAGES],
-    userPermissions: [Permissions.FLAGS.ADMINISTRATOR]
+  clientPermissions: [Permissions.FLAGS.SEND_MESSAGES],
+  userPermissions: [Permissions.FLAGS.ADMINISTRATOR],
 };
 
 module.exports.data = new SlashCommandBuilder()
-    .setName("setup")
-    .setDescription("Setup the bot.")
-    .addRoleOption(option => option.setName("role").setDescription("The role you wish to be pinged on a bot status change.").setRequired(true))
-    .addChannelOption(option => option.setName("channel").setDescription("The channel you wish to be notified in about bot status updates.").setRequired(true));
+  .setName("setup")
+  .setDescription("Setup the bot.")
+  .addRoleOption((option) =>
+    option
+      .setName("role")
+      .setDescription("The role you wish to be pinged on a bot status change.")
+      .setRequired(true)
+  )
+  .addChannelOption((option) =>
+    option
+      .setName("channel")
+      .setDescription(
+        "The channel you wish to be notified in about bot status updates."
+      )
+      .setRequired(true)
+  );
